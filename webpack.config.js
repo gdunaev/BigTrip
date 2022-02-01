b239a9fddb510bf88e1713294a4b49db1bce91af
Gleb Dunaev <7-v-7@bk.ru>
2022-02-01 17:27:16 +0300

    1.1 и 1.2 пункты задания

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  devServer: {
+    hot: false,
+  },
+  mode: 'development',
+};
