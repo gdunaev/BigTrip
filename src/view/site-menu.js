06e70d6c5fc9e84ab6c8634ee2de4e71a7b4626a
Gleb Dunaev~ <7-v-7@bk.ru~>
2022-04-06 19:05:47 +0300

    7.2.3 добавляет переключатель список/статистика

--- src/view/site-menu.js
+++ src/view/site-menu.js
@@ -2,14 +2,13 @@ import {AbstractView} from "./abstract.js";
 import {MenuItem} from './const.js';
 
 const createNavigationTemplate = (currentMenuItem) => {
-  console.log('33',MenuItem.STATS === currentMenuItem);
+  // console.log('33',MenuItem.STATS === currentMenuItem);
     return `<nav class="trip-controls__trip-tabs  trip-tabs">
-    <a class="trip-tabs__btn ${MenuItem.TABLE === currentMenuItem ? 'trip-tabs__btn--active': ''}" value=${MenuItem.TABLE} href="#">${MenuItem.TABLE}</a>
-    <a class="trip-tabs__btn ${MenuItem.STATS === currentMenuItem ? 'trip-tabs__btn--active': ''}" value=${MenuItem.STATS} href="#">${MenuItem.STATS}</a>
+    <a class="trip-tabs__btn trip-tabs__btn--active" value=${MenuItem.TABLE} href="#">${MenuItem.TABLE}</a>
+    <a class="trip-tabs__btn" value=${MenuItem.STATS} href="#">${MenuItem.STATS}</a>
   </nav>`;
 
 };
-
 
 
 export default class SiteMenuView extends AbstractView {
@@ -27,27 +26,17 @@ export default class SiteMenuView extends AbstractView {
 
   _menuClickHandler(evt) {
     const menuItem = MenuItem[evt.target.textContent.toUpperCase()];
-    // console.log('22', MenuItem[evt.target.textContent.toUpperCase()])
-    // this._currentMenuItem = MenuItem[evt.target.textContent.toUpperCase()];
-    // this.getElement();
+
+    const items = this.getElement().querySelectorAll('.trip-tabs__btn');
+    items.forEach((element) => {element.textContent === menuItem ? element.className = 'trip-tabs__btn trip-tabs__btn--active' : element.className = 'trip-tabs__btn' });
+
     evt.preventDefault();
     this._callback.menuClick(menuItem);
   }
 
   setMenuClickHandler(callback) {
-    // console.log('22')
     this._callback.menuClick = callback;
     this.getElement().addEventListener('click', this._menuClickHandler);
-  }
-
-  setMenuItem(menuItem) {
-    // console.log('33', menuItem)
-
-    const item = this.getElement().querySelector(`[value=${menuItem}]`);
-
-    if (item !== null) {
-      item.checked = true;
-    }
   }
 }
 
